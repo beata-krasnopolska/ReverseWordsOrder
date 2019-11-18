63d7ecc6d2d2b1a46b90a60cb28460cdcc50b395
beata.krasnopolska@gmail.com <beata.krasnopolska@gmail.com>
2019-11-18 19:51:36 +0100

    Add project files.

---
+++ ReverseWordsOrder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseWordsOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program takes a string of one or more words as an argument and returns the same string, " +
+                                "but with all five or more letter words reversed. " +
+                                "Strings passed in consist of only letters and spaces.");
+
+            string sentence_1 = "Reverse";
+            string sentence_2 = "This is a typical sentence.";
+            string sentence_3 = "The dog is big.";
+
+            var reverser = new Reverser();
+            var reverseWords = reverser.ReverseOrder(sentence_2);
+
+            foreach (var i in reverseWords)
+            {
+                Console.Write(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ReverseWordsOrder/Reverser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseWordsOrder
+{
+    class Reverser
+    {
+        public string ReverseOrder(string sentence)
+        {
+            var words = sentence.Split(' '); //.Where(c => c.Length > 5).Reverse();
+            //var toReverse = words.Where(c => c.Length > 5).Reverse();
+            foreach (var word in words)
+            {
+                if (word.Length > 5)
+                {
+                    char[] charArray = word.ToCharArray();
+                    Array.Reverse(charArray);
+                    return new string(charArray);
+                }
+                else
+                {
+                    return word;
+                }
+            }
+            
+            var finalString = String.Join(" ", words);
+            return finalString;
+        }
+    }
+}
